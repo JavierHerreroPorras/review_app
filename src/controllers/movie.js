23e43cd465e96c00027efe4e64a633294d49e2fe
Javier Herrero <javierherreroporras@gmail.com>
2023-09-13 22:58:00 +0200

    Add new controllers for movies

---
+++ src/controllers/movie.js
@@ -0,0 +1,64 @@
+const axios = require('axios');
+
+async function searchMovie(req, res) {
+    const { title } = req.query || {};
+
+    try {
+        if (title) {
+            const apiRes = await axios.get(`http://www.omdbapi.com/?s=${title}&apikey=`);
+
+            if (apiRes.data.Response === 'True') {
+                return res.json(apiRes.data.Search.map(item => {
+                    return {
+                        Title: item.Title,
+                        Year: item.Year,
+                        ExternalID: item.imdbID,
+                        Type: item.Type,
+                        Image: item.Poster
+                    }
+                }));
+            }
+        }
+
+    } catch (error) {
+        //console.log(error);
+    }
+
+    return res.json([]);
+}
+
+async function getMovie(req, res) {
+    var movie = {}
+    const externalID = req.params.id;
+
+    try {
+        if (externalID) {
+            var apiRes = await axios.get(`http://www.omdbapi.com/?i=${externalID}&apikey=`);
+            if (apiRes.data.Response === 'True') {
+                movie = apiRes.data;
+
+                return res.json(
+                    {
+                        Title: movie.Title,
+                        Year: movie.Year,
+                        Genre: movie.Genre,
+                        Director: movie.Director,
+                        Writer: movie.Writer,
+                        Actors: movie.Actors,
+                        Description: movie.Plot,
+                        Image: movie.Poster,
+                        ExternalID: movie.imdbID,
+                        Type: movie.Type,
+                        Seasons: movie.totalSeasons
+                    }
+                );
+            }
+        }
+    } catch (error) {
+        //console.log(error);
+    }
+
+    return res.json({});
+}
+
+module.exports = { searchMovie, getMovie };
